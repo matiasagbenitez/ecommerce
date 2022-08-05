5d04053f0fea548211e87418ece721ee3f9e3203
Matías <matiasagbenitez@gmail.com>
2022-08-05 15:36:35 -0300

    Colors added to TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -14,6 +15,16 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                black: colors.black,
+                white: colors.white,
+                gray: colors.gray,
+                emerald: colors.emerald,
+                indigo: colors.indigo,
+                yellow: colors.yellow,
+              },
         },
     },
 
