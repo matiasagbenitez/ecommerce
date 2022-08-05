9bd46e115565279d8910f5617f24e53c45aa0334
Matías <matiasagbenitez@gmail.com>
2022-08-05 17:00:45 -0300

    FontAwesome icons kit added to the project

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -16,6 +16,63 @@
         {{-- Barra de búsqueda + botón --}}
         @livewire('search')
 
+        <div class="ml-3 relative">
+
+            @auth
+                {{-- USUARIO AUTENTICADO --}}
+                <x-jet-dropdown align="right" width="48">
+
+                    <x-slot name="trigger">
+                        <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                            <img class="h-8 w-8 rounded-full object-cover mx-2" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <!-- Account Management -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            {{ __('Manage Account') }}
+                        </div>
+
+                        <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                            {{ __('Profile') }}
+                        </x-jet-dropdown-link>
+
+                        <div class="border-t border-gray-100"></div>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}" x-data>
+                            @csrf
+
+                            <x-jet-dropdown-link href="{{ route('logout') }}"
+                                    @click.prevent="$root.submit();">
+                                {{ __('Log Out') }}
+                            </x-jet-dropdown-link>
+                        </form>
+                    </x-slot>
+
+                </x-jet-dropdown>
+            @else
+                {{-- USUARIO NO AUTENTICADO --}}
+                <x-jet-dropdown align="right" width="48">
+
+                    <x-slot name="trigger">
+                        <i class="fas fa-user-circle text-white text-3xl mx-2 cursor-pointer"></i>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-jet-dropdown-link href="{{ route('login') }}">
+                            {{ __('Login') }}
+                        </x-jet-dropdown-link>
+
+                        <x-jet-dropdown-link href="{{ route('register') }}">
+                            {{ __('Register') }}
+                        </x-jet-dropdown-link>
+                    </x-slot>
+
+                </x-jet-dropdown>
+            @endauth
+        </div>
 
     </div>
 </header>
