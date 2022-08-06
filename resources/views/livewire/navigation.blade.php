3390aab29eb1c5373c34558986d28e6c4b6dc96f
Matías <matiasagbenitez@gmail.com>
2022-08-06 15:48:14 -0300

    Set the settings bar to be on top of everything

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,4 +1,4 @@
-<header x-data="dropdown()" class="bg-neutral-600 sticky top-0">
+<header x-data="dropdown()" class="bg-neutral-600 sticky top-0 z-50">
 
     {{-- Barra superior --}}
     <div class="container flex items-center h-16 justify-between md:justify-start">
