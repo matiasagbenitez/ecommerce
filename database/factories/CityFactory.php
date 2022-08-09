b2368eeff40a5e63b6318c5efc921d164ee3eaf8
Matías <matiasagbenitez@gmail.com>
2022-08-09 10:15:43 -0300

    Factories and Seeder for order management finished

--- database/factories/CityFactory.php
+++ database/factories/CityFactory.php
@@ -4,20 +4,13 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\City>
- */
 class CityFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word(),
+            'cost' => $this->faker->randomElement([5, 10, 15])
         ];
     }
 }
